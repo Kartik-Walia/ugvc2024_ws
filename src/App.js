0ba7045acfcdfe806341b6e69be1a50def2ef66f
Kartik-Walia <itzkartikeds3@gmail.com>
2024-07-26 13:48:19 +0300

    Added manual routing

--- src/App.js
+++ src/App.js
@@ -1,101 +1,16 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { FaArrowRight, FaArrowLeft, FaArrowUp, FaArrowDown } from "react-icons/fa";
+import React from 'react';
+import Manual from './components/Manual';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
-  const [speed, setSpeed] = useState(30);
-
-  const handleClick = (url) => {
-    axios.get(url)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
-  const handleSliderChange = (event) => {
-    setSpeed(event.target.value);
-    axios.get(`http://192.168.0.101:5000/speed/${event.target.value}`)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
   return (
-    <div className="container">
-      <div className="header">
-        <h1 className="title">AGNI CONTROLLER</h1>
-      </div>
-
-      <div className='container-body'>
-        {/* Control buttons */}
-        <div className="controls">
-
-          {/* Forwards */}
-          <div className="button-wrapper">
-            <button
-              className="control-button"
-              onClick={() => handleClick('http://192.168.0.101:5000/start')}
-            >
-              <FaArrowUp />
-            </button>
-          </div>
-
-          {/* Left Start/Stop Right */}
-          <div className="button-group">
-            <button
-              className="control-button"
-              onClick={() => handleClick('http://192.168.0.101:5000/left')}
-            >
-              <FaArrowLeft />
-            </button>
-            <button
-              className="control-button stop-button"
-              onClick={() => handleClick('http://192.168.0.101:5000/stop')}
-            >
-              Stop
-            </button>
-            <button
-              className="control-button"
-              onClick={() => handleClick('http://192.168.0.101:5000/right')}
-            >
-              <FaArrowRight />
-            </button>
-          </div>
-
-          {/* Reverse */}
-          <div className="button-wrapper">
-            <button
-              className="control-button"
-              onClick={() => handleClick('http://192.168.0.101:5000/reverse')}
-            >
-              <FaArrowDown />
-            </button>
-          </div>
-        </div>
-
-        <div className="slider-wrapper">
-          <div className="slider-title">Speed Controller</div>
-          <input
-            type="range"
-            min="0"
-            max="255"
-            value={speed}
-            className="slider"
-            onChange={handleSliderChange}
-          />
-          <div className="slider-value">{speed}</div>
-        </div>
-      </div>
-
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/manual' element={<Manual />} />
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
---
+++ src/components/Manual.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { FaArrowRight, FaArrowLeft, FaArrowUp, FaArrowDown } from "react-icons/fa";
+import '../App.css';
+
+const Manual = () => {
+  const [speed, setSpeed] = useState(30);
+
+  const handleClick = (url) => {
+    axios.get(url)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleSliderChange = (event) => {
+    setSpeed(event.target.value);
+    axios.get(`http://192.168.0.101:5000/speed/${event.target.value}`)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <h1 className="title">AGNI MANUAL CONTROLLER</h1>
+      </div>
+
+      <div className='container-body'>
+        {/* Control buttons */}
+        <div className="controls">
+
+          {/* Forwards */}
+          <div className="button-wrapper">
+            <button
+              className="control-button"
+              onClick={() => handleClick('http://192.168.0.101:5000/start')}
+            >
+              <FaArrowUp />
+            </button>
+          </div>
+
+          {/* Left Start/Stop Right */}
+          <div className="button-group">
+            <button
+              className="control-button"
+              onClick={() => handleClick('http://192.168.0.101:5000/left')}
+            >
+              <FaArrowLeft />
+            </button>
+            <button
+              className="control-button stop-button"
+              onClick={() => handleClick('http://192.168.0.101:5000/stop')}
+            >
+              Stop
+            </button>
+            <button
+              className="control-button"
+              onClick={() => handleClick('http://192.168.0.101:5000/right')}
+            >
+              <FaArrowRight />
+            </button>
+          </div>
+
+          {/* Reverse */}
+          <div className="button-wrapper">
+            <button
+              className="control-button"
+              onClick={() => handleClick('http://192.168.0.101:5000/reverse')}
+            >
+              <FaArrowDown />
+            </button>
+          </div>
+        </div>
+
+        <div className="slider-wrapper">
+          <div className="slider-title">Speed Controller</div>
+          <input
+            type="range"
+            min="0"
+            max="255"
+            value={speed}
+            className="slider"
+            onChange={handleSliderChange}
+          />
+          <div className="slider-value">{speed}</div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Manual;
